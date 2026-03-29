213d8def59510f3f46da857846ea11bd187e08aa
oliveiradiogomf <oliveira.diogo.mf@gmail.com>
2026-03-29 17:45:04 +0100

    SchoolStock

---
+++ novo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>SchoolStock</title>
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <link href="images/logo1.png" rel="shortcut icon" />
+</head>
+
+<body>
+
+    <div class="content">
+	
+	</div>
+	
+</body>
+</html>
